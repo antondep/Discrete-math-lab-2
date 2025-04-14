d7eaaf91fa3d89b5015206b7175e95219ffaa4f6
rasthpop <rasthetrollman@example.com>
2025-04-14 14:07:43 +0300

    pruned encryption\decryption

--- rsa.py
+++ rsa.py
@@ -24,7 +24,7 @@ def generate_keys():
         q = random.choice(primes)
     n = p * q
     phi = (p-1)*(q-1)
-    e = 65537 
+    e = 65537
     while math.gcd(e, phi) != 1:
         e = random.randint(3, phi - 1)
     d = modinv(e, phi)
@@ -34,10 +34,21 @@ def generate_keys():
 
 
 def encrypt_message(message: str, enc_key: tuple):
-    codes = [ord(el) for el in message]
-    encoded_symbols = [(el ** enc_key[0]) % enc_key[1] for el in codes]
 
-    return encoded_symbols
+    codes = [ord(el) for el in message]
+    encoded_symbols = [str((el ** enc_key[0]) % enc_key[1]) for el in codes]
+
+    return " ".join(encoded_symbols) if encoded_symbols else None
 
 def decrypt_message(cypher, private_key):
+    cypher = [int(el) for el in cypher.split()]
     return "".join([chr((el ** private_key[0]) % private_key[1]) for el in cypher])
+
+
+if __name__ == "__main__":
+    e, d =  generate_keys()
+    m = encrypt_message("I WANT to bReak free!", e)
+
+    print(m)
+
+    print(decrypt_message(m, d))
